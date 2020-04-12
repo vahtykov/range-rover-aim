be73b6fc94c8d3be35760efd1477b822d75507a4
Ilya <vahtykov.ilya@bk.ru>
2020-04-13 00:58:52 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import range from './range.jpg';
 import './App.css';
 
 function App() {
+  const [_salary, _setSalary] = useState(40000);
+  const [_moneyNow, _setMoneyNow] = useState(120000);
+  const [_roverPrice, _setRoverPrice] = useState(600000);
+
   return (
     <div className="App">
       <h1>
@@ -12,8 +16,15 @@ function App() {
       <img src={range} alt='Так выглядит мечта' className='rangeImg' />
 
       <h2 className='rangeSumm'>
-        { `${parseFloat('70000').toLocaleString()} ₽` } / { `${parseFloat('600000').toLocaleString()} ₽` }
+        { `${_moneyNow.toLocaleString()} ₽` } / { `${_roverPrice.toLocaleString()} ₽` }
       </h2>
+
+      <h3>До RR Vogue, при зп { `${_salary.toLocaleString()} ₽` } в месяц, осталось: { ((_roverPrice - _moneyNow)/_salary).toFixed(1) } месяцев</h3>
+
+      <label>
+        Текущая зп в месяц:
+        <input value={_salary} onChange={(e) => _setSalary(parseInt(e.target.value))} className='input' type='number' />
+      </label>
     </div>
   );
 }
